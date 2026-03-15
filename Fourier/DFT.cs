ef0b133b06457f2a4995a3e1b0b4b5ddf075df0d
jpmikkers <jean.paul.mikkers@gmail.com>
2026-03-15 16:58:39 +0100

    Add project files.

---
+++ Fourier/DFT.cs
@@ -0,0 +1,34 @@
+﻿namespace Fourier;
+
+using System;
+using System.Numerics;
+
+public static class DFT
+{
+    /// <summary>
+    /// returns n-th roots of unity, the points of a regular n-gon on the unit circle in the complex plane.
+    /// (see https://algebrica.org/roots-of-unity/ )
+    /// </summary>
+    /// <param name="n">how many points</param>
+    /// <param name="direction">1 for counter clockwise, -1 for clockwise</param>
+    /// <returns></returns>
+    private static Complex[] RootsOfUnity(int n, int direction)
+        => [.. Enumerable.Range(0, n).Select(i => Complex.FromPolarCoordinates(1, direction * i * Math.Tau / n))];
+
+    public static Complex[] DiscreteFourierTransform(Complex[] dft, bool forward)
+    {
+        var n = dft.Length;
+
+        // precalculate zetas, clockwise roots of unity for forward DFT, counterclockwise for reverse DFT
+        var zetas = RootsOfUnity(n, forward ? -1 : 1);
+        var scaleFactor = forward ? 1.0 : 1.0 / n;
+
+        return
+            [.. Enumerable.Range(0, n)
+            .Select(i =>
+                dft.Select((sample, j) => sample * zetas[(i * j) % n])
+                .Aggregate(Complex.Zero, Complex.Add) * scaleFactor
+            )
+            ];
+    }
+}
---
+++ Fourier/Program.cs
@@ -0,0 +1,33 @@
+﻿using Fourier;
+using ScottPlot;
+using System.Numerics;
+
+var signal = new double[1024];
+for (var i = 0; i < signal.Length; i++)
+{
+    signal[i] =
+        Math.Sin(i * 20 * Math.Tau / signal.Length)
+        + Math.Sin(i * 40 * Math.Tau / signal.Length)
+        + Math.Sin(i * 80 * Math.Tau / signal.Length)
+        + Math.Sin(i * 160 * Math.Tau / signal.Length)
+        + Math.Sin(i * 320 * Math.Tau / signal.Length);
+}
+
+var plot1 = new Plot();
+plot1.Add.Signal(signal);
+plot1.Title("Signal");
+plot1.SavePng("signal.png", 1024, 768);
+
+var spectrum = DFT.DiscreteFourierTransform([.. signal.Select(x => (Complex)x)], forward: true);
+
+var plot2 = new Plot();
+plot2.Add.Signal(spectrum.Select(Complex.Abs).ToList());
+plot2.Title("Spectrum");
+plot2.SavePng("spectrum.png", 1024, 768);
+
+var reconstructed = DFT.DiscreteFourierTransform(spectrum, forward: false).Select(x => x.Real).ToArray();
+
+var plot3 = new Plot();
+plot3.Add.Signal(reconstructed);
+plot3.Title("Reconstructed signal");
+plot3.SavePng("reconstructed.png", 1024, 768);
